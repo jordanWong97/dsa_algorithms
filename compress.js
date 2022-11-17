0172bebe0a001cd7d0967052f8e1be2bda33a234
Jordan Wong <pjwong97@gmail.com>
2022-11-16 16:17:43 -0800

    init commit

---
+++ compress.js
@@ -0,0 +1,23 @@
+const compress = (s) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (j <= s.length) {
+    if (s[j] === s[i]) {
+      j++;
+    } else {
+      let num = j - i;
+      if (num === 1) {
+        result.push(s[i]);
+      } else {
+        result.push(`${num}${s[i]}`);
+      }
+      i = j;
+    }
+  }
+  return result.join('');
+};
+
+module.exports = {
+  compress,
+};
