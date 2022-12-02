314c6d29934e896c085a370d5668a50bdb876f34
Jordan Wong <pjwong97@gmail.com>
2022-12-01 16:16:11 -0800

    complete with recursive approach

--- linkedListValues.js
+++ linkedListValues.js
@@ -16,6 +16,20 @@ const linkedListValues = (head) => {
   return result;
 };
 
+//recursive break into 2 functions
+
+// const linkedListValues = (head) => {
+//   let values = [];
+//   fillValues(head,values);
+//   return values;
+// };
+
+// const fillValues = (head, values) => {
+//   if(head === null) return;
+//   values.push(head.val);
+//   fillValues(head.next, values)
+// }
+
 module.exports = {
   linkedListValues,
 };
