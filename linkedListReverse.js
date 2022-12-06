e6b396259b07b1abf13d5266a6ca6d7adb48042c
Jordan Wong <pjwong97@gmail.com>
2022-12-05 17:16:00 -0800

    reverse linked list

---
+++ linkedListReverse.js
@@ -0,0 +1,25 @@
+const reverseList = (head) => {
+  let current = head;
+  let prev = null;
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
+
+
+//recursive
+// const reverseList = (head, prev = null) => {
+//   if(head === null) return prev;
+//   const next = head.next;
+//   head.next = prev;
+//   return reverseList(next, head);
+// };
+
+module.exports = {
+  reverseList,
+};
+
