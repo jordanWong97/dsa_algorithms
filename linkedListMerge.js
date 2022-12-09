eb6744e3cf98f7b7c53a7214283ccbcd018b25e0
Jordan Wong <pjwong97@gmail.com>
2022-12-08 18:39:52 -0800

    merge recursively

--- linkedListMerge.js
+++ linkedListMerge.js
@@ -21,3 +21,24 @@ const mergeLists = (head1, head2) => {
   return tempHead.next;
 };
 
+
+//recursively
+
+// const mergeLists = (head1, head2) => {
+//   if(head1 === null && head2 === null) return null;
+//   if(head1 === null) return head2;
+//   if(head2 === null) return head1;
+//   if(head1.val < head2.val){
+//     const nextHead1 = head1.next;
+//     head1.next = mergeLists(nextHead1, head2);
+//     return head1;
+//   } else{
+//     const nextHead2 = head2.next;
+//     head2.next = mergeLists(head1, nextHead2)
+//     return head2;
+//   }
+// }
+
+module.exports = {
+  mergeLists,
+};
