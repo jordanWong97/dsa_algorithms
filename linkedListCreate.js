c3ebe287610763dd9b332f3720e0ce44f1bd0ef6
Jordan Wong <pjwong97@gmail.com>
2022-12-26 11:30:33 -0800

    recursive create linked list

--- linkedListCreate.js
+++ linkedListCreate.js
@@ -11,6 +11,18 @@ const createLinkedList = (values) => {
   return head.next;
 };
 
+//resursive
+
+//must pre-increment when passing argument in recursive function, or will
+//increment after assignment
+
+// const createLinkedList = (values, i = 0) => {
+//   if(i === values.length) return null;
+//   const head = new Node(values[i]);
+//   head.next = createLinkedList(values, ++i);
+//   return head;
+// }
+
 module.exports = {
   createLinkedList,
 };
