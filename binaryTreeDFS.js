fed7f7a9c89fcc6fbb116422ea1923e85c4d0080
Jordan Wong <pjwong97@gmail.com>
2022-12-27 17:20:13 -0800

    recursive

--- binaryTreeDFS.js
+++ binaryTreeDFS.js
@@ -15,6 +15,22 @@ const depthFirstValues = (root) => {
   return values;
 };
 
+//recursive
+
+//checks all left recursively, then checks right for each node
+// return [root val, spread all left values then right]
+
+// const depthFirstValues = (root) => {
+//   if(root === null) return [];
+
+//   const left = depthFirstValues(root.left);
+//   const right = depthFirstValues(root.right);
+
+//   return [root.val, ...left, ...right]
+// }
+
+
+
 module.exports = {
   depthFirstValues,
 };
