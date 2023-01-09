36b34ac368cd89c4a3b69e17183a113604572034
Jordan Wong <pjwong97@gmail.com>
2023-01-09 14:38:46 -0800

    recursive dfs and bfs

--- binaryTreeTargetCount.js
+++ binaryTreeTargetCount.js
@@ -17,6 +17,34 @@ const treeValueCount = (root, target) => {
   return count;
 };
 
+//bfs
+// const treeValueCount = (root, target) => {
+//     // todo
+//   if(root === null) return 0;
+
+//   let queue = [root];
+//   let count = 0;
+
+//   while(queue.length !== 0){
+//     const node = queue.shift();
+//     if(node.val === target){
+//       count++;
+//     }
+//     if(node.left !== null) queue.push(node.left);
+//     if(node.right !== null) queue.push(node.right);
+//   }
+//   return count;
+// }
+
+//dfs recursive
+// const treeValueCount = (root, target) => {
+//   if(root === null) return 0;
+
+//   const match = root.val === target ? 1 : 0
+
+//   return match + treeValueCount(root.left, target) + treeValueCount(root.right, target);
+// }
+
 module.exports = {
   treeValueCount,
 };
