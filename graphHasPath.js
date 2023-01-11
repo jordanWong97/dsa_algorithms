32bc91e3bb32917fe36da6e858ec4690fe206929
Jordan Wong <pjwong97@gmail.com>
2023-01-10 17:59:40 -0800

    bfs check for path from src to destination

--- graphHasPath.js
+++ graphHasPath.js
@@ -14,6 +14,25 @@ const hasPath = (graph, src, dst) => {
   return false;
 };
 
+//bfs iterative
+// const hasPath = (graph, src, dst) => {
+
+//   let queue = [src];
+
+//   while(queue.length !== 0){
+//     const current = queue.shift();
+//     if(current === dst) return true;
+
+//     //pushing from values in object
+//     for(let neighbor of graph[current]){
+//       queue.push(neighbor);
+//     }
+//   }
+//   return false;
+// }
+
+
+
 module.exports = {
   hasPath,
 };
