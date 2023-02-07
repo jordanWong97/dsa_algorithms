33cdf6060ba5f9c4ab04501663800bd1a5840dd6
Jordan Wong <pjwong97@gmail.com>
2023-02-06 16:47:03 -0800

    add lists together recursively, reverse order

--- linkedListAddLists.js
+++ linkedListAddLists.js
@@ -1,25 +1,24 @@
+//recursive
+const addLists = (head1, head2, carry = 0) => {
+  if (head1 === null && head2 === null && carry === 0) return null;
 
+  //check if values exist, if not return 0 for either instead of null
+  const v1 = head1 === null ? 0 : head1.val;
+  const v2 = head2 === null ? 0 : head2.val;
 
-const addLists = (head1, head2) => {
-  // todo
-  let current1 = head1;
-  let current2 = head2;
-  let sumHead = new Node(null);
-  let currentSum = sumHead;
+  //sum is sum of a column, digit stays, carry over as 1 to .next
+  const sum = v1 + v2 + carry;
+  const nextCarry = sum > 9 ? 1 : 0;
+  const digit = sum % 10;
 
-  while (current1 !== null && current2 !== null) {
-    if (current1 === null) {
-      current1.val = 0;
-    }
-    if (current2 === null) {
-      current2.val = 0;
-    }
-    currentSum.next = new Node(current1.val + current2.val);
-    currentSum = currentSum.next;
-    current1 = current1.next;
-    current2 = current2.next;
-  }
-  return sumHead.next;
+  const result = new Node(digit);
+
+  const next1 = head1 === null ? null : head1.next;
+  const next2 = head2 === null ? null : head2.next;
+
+  result.next = addLists(next1, next2, nextCarry);
+
+  return result;
 };
 
 module.exports = {
